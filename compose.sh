a1f0a0016b4a22483e0dbd7bde9e2d45a828796b
cyberjon <cyberjon09@gmail.com>
2018-09-19 01:51:55 +0800

    Update compose.sh

--- compose.sh
+++ compose.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #Install compose 0.19.4
 su - hyper <<EOF
+echo "" > $(npm config get globalconfig)
+npm config --global edit
 npm uninstall -g composer-cli
 npm install -g composer-cli@0.19.4
 EOF
